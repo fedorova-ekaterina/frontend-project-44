27034975cf29f591e928935263b8cf6c785d8503
ekaterina fedorova <fedorova.ekaterina@hexly.ru>
2023-08-23 02:05:49 +0300

    step-6

---
+++ src/games/calc.js
@@ -0,0 +1,43 @@
+import { getRandomNumber } from '../utils.js';
+
+import runGameLogic, { roundsNumber } from '../gameLogic.js';
+
+const description = 'What is the result of the expression?';
+
+const getRandomOperator = () => {
+  const operators = ['+', '-', '*'];
+  const randOperatorIndex = getRandomNumber(0, operators.length);
+  return operators[randOperatorIndex];
+};
+
+const calculate = (operator, operand1, operand2) => {
+  if (operator === '+') {
+    return operand1 + operand2;
+  }
+  if (operator === '-') {
+    return operand1 - operand2;
+  }
+  if (operator === '*') {
+    return operand1 * operand2;
+  }
+  return Error('Incorrect operator');
+};
+
+const generateRound = () => {
+  const firstOperand = getRandomNumber(1, 100);
+  const secondOperand = getRandomNumber(1, 100);
+  const operator = getRandomOperator();
+  const question = `${firstOperand} ${operator} ${secondOperand}`;
+  const correctAnswer = String(calculate(operator, firstOperand, secondOperand));
+  return [question, correctAnswer];
+}
+
+const runCalcGame = () => {
+  const rounds = [];
+  for (let i = 0; i < roundsNumber; i += 1) {
+    rounds[i] = generateRound();
+  }
+  return runGameLogic(rounds, description);
+};
+
+export default runCalcGame;
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,2 @@
+// eslint-disable-next-line
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
