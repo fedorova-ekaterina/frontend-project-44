3638a8bb1dbcfe0bbc05e887a3d46ea68a22758e
ekaterina fedorova <fedorova.ekaterina@hexly.ru>
2023-08-25 23:29:26 +0300

    correct-3

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -5,7 +5,7 @@ export const roundsNumber = 3;
 const runGameLogic = (rounds, description) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello ${userName}!`);
+  console.log(`Hello, ${userName}!`);
   console.log(description);
   for (let i = 0; i < roundsNumber; i += 1) {
     const [question, answer] = rounds[i];
